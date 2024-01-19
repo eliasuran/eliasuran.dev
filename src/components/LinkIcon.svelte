c15ba110d3a84f6a9795a151f915630ff4b3c649
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-19 22:58:39 +0100

    learning astro stuff, added layout and getting projects

---
+++ src/components/LinkIcon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Icon from "@iconify/svelte";
+  export let href: string;
+  export let icon: string;
+</script>
+
+<a {href}>
+  <Icon {icon} height={30} class="" />
+</a>
