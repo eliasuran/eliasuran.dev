9f5ccd1dc61dc115bd4880321ddb22e7a6c11c02
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-20 01:21:23 +0100

    changed linkicon from svelte to astro

--- src/components/LinkIcon.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import Icon from "@iconify/svelte";
-  export let href: string;
-  export let icon: string;
-</script>
-
-<a {href}>
-  <Icon {icon} height={30} class="" />
-</a>
