e57d915c8a5313d5c73b538b1a7052e09d9f19d3
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-20 20:06:31 +0100

    added theme for posts and displaying posts on home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
+import rosepine from "./src/lib/rose-pine.json"
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  markdown: { shikiConfig: { theme: rosepine, wrap: true}}
 });
