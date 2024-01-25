43ec4fd15aabfef579588858d597439a339b7a76
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-25 11:12:11 +0100

    changed navitem back to svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import rosepine from "./src/lib/rose-pine.json";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), svelte()],
   markdown: {
     shikiConfig: {
       theme: rosepine,
