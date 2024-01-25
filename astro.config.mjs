1ea0a613cf1893c757e7d4e19794b8dc90b74bb2
Elias Rissanen Uran <elura001@osloskolen.no>
2024-01-25 10:31:14 +0100

    changed nav items to astro from svelte, sorting posts by date

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import rosepine from "./src/lib/rose-pine.json";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), svelte()],
+  integrations: [tailwind(), icon()],
   markdown: {
     shikiConfig: {
       theme: rosepine,
