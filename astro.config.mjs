558784c46cca6bef004033c470dec08371842d9a
Elias Rissanen Uran <elura001@osloskolen.no>
2024-03-02 19:08:26 +0100

    added hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import rosepine from "./src/lib/rose-pine.json";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.eliasuran.dev",
   integrations: [tailwind(), icon(), svelte()],
   markdown: {
     shikiConfig: {
       theme: rosepine,
       wrap: true
     }
-  }
+  },
+  output: "hybrid",
+  adapter: vercel()
 });
